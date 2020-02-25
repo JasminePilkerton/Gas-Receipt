487c53e93ce6cbbed287247be7ceb51a78dcec86
Jasmine <jasmine.pilkerton.2022@student.lhschools.org>
2020-02-25 12:04:02 -0500

    Intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,31 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-------------------------+");
+        System.out.println("|Jazz's Gas Station       |");
+        System.out.println("|1234 North ST            |");
+        System.out.println("|Las Vegas                |");
+        System.out.println("|NV 89102                 |");
+        System.out.println("|Date          Time       |");
+        System.out.println("|01/16/20     12:31PM     |");
+        System.out.println("|Invoice#     645387      |");
+        System.out.println("|Auth#                    |");
+        System.out.println("|Mastercard               |");
+        System.out.println("|Account Number           |");
+        System.out.println("|*********7689            |");
+        System.out.println("|Pump      Product   $/G  |");
+        System.out.println("|06        UNLD      $2.6 |");
+        System.out.println("|Gallons        Fuel Total|");
+        System.out.println("|20.69            $60.00  |");
+        System.out.println("|Thank You!               |");
+        System.out.println("|Jazz's Gas Station       |");
+        System.out.println("+-------------------------+");
+
+
+
+
+        //
+    }
+}
